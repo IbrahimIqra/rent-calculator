c50dc6f4cb3ca6f1f6d51ee1337e089b09f18acc
Ibrahim Iqra <ibrahimiqra1@gmail.com>
2021-11-17 12:49:51 +0600

    created sample csv file and read from it

--- sketch.js
+++ sketch.js
@@ -1,7 +1,36 @@
+
+let flat_info;
+let start_btn;
+let started, meter_num;
+
+function preload(){
+
+  flat_info = loadTable('rents.csv','csv','header');
+
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+
+  start_btn = createButton('Start').size(80,50);
+  start_btn.center();
 }
 
 function draw() {
   background(220);
+
+  if (!started){
+    start_btn.mousePressed(starting);
+  }
+  else{
+
+    elec_bill = window.prompt("Input Electric Bill: ");
+
+  }
+}
+
+function starting(){
+  started=true;
+  start_btn.hide();
+  meter_num = window.prompt("Input Meter No.: ");
 }
